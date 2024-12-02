341dc4fc75a21834ed7c2b10ba7b65786a101a49
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2024-12-02 08:49:31 +0300

    day2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <img src="./.assets/christmas_ferris.png" width="164">
 
-# 🎄 Advent of Code {year}
+# 🎄 Advent of Code 2024
 
 Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.rust-lang.org/).
 
@@ -12,7 +12,8 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
 | [Day 1](./src/bin/01.rs) | `24.9µs` | `16.0µs` |
+| [Day 2](./src/bin/02.rs) | `117.5µs` | `131.6µs` |
 
-**Total: 0.04ms**
+**Total: 0.29ms**
 <!--- benchmarking table --->
 
---
+++ src/bin/02.rs
@@ -0,0 +1,90 @@
+use std::cmp::Ordering;
+
+use itertools::Itertools;
+use nom::{
+    character::complete::{newline, space1, u32},
+    multi::separated_list1,
+    IResult,
+};
+
+advent_of_code::solution!(2);
+
+fn parse_line(input: &[u8]) -> IResult<&[u8], Vec<u32>> {
+    separated_list1(space1, u32)(input)
+}
+
+fn parse_file(input: &[u8]) -> nom::IResult<&[u8], Vec<Vec<u32>>> {
+    (separated_list1(newline, parse_line))(input)
+}
+
+fn is_safe_1(list: &[u32]) -> bool {
+    let list = list.iter().tuple_windows();
+    let mut ordering = Ordering::Equal;
+    for (a, b) in list {
+        let c = a.cmp(b);
+        ordering = ordering.then(c);
+        if !(c == ordering && (1..=3).contains(&a.abs_diff(*b))) {
+            return false;
+        }
+    }
+    true
+}
+
+fn is_safe_2(list: &[u32]) -> bool {
+    if is_safe_1(list) {
+        return true;
+    }
+
+    (0..list.len()).any(|doomed_idx| {
+        let list = list
+            .iter()
+            .enumerate()
+            .filter(|(i, _)| *i != doomed_idx)
+            .map(|(_, v)| v)
+            .tuple_windows();
+        let mut ordering = Ordering::Equal;
+        for (a, b) in list {
+            let c = a.cmp(b);
+            ordering = ordering.then(c);
+            if !(c == ordering && (1..=3).contains(&a.abs_diff(*b))) {
+                return false;
+            }
+        }
+        true
+    })
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let input = input.as_bytes();
+    let lists = parse_file(input).ok()?.1;
+
+    let res = lists.into_iter().filter(|v| is_safe_1(v)).count();
+
+    Some(res as u32)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let input = input.as_bytes();
+    let lists = parse_file(input).ok()?.1;
+
+    let res = lists.into_iter().filter(|v| is_safe_2(v)).count();
+
+    Some(res as u32)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(4));
+    }
+}

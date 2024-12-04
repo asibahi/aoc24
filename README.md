f4e3952d7a6b1aedc7efc3e417c4710a6ddb6a78
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2024-12-04 14:01:07 +0300

    day4 perf + added (slow) nom solution

--- README.md
+++ README.md
@@ -14,8 +14,8 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | [Day 1](./src/bin/01.rs) | `22.7µs` | `16.0µs` |
 | [Day 2](./src/bin/02.rs) | `56.3µs` | `68.5µs` |
 | [Day 3](./src/bin/03.rs) | `16.0ns` | `17.0ns` |
-| [Day 4](./src/bin/04.rs) | `74.0µs` | `37.2µs` |
+| [Day 4](./src/bin/04.rs) | `66.3µs` | `24.0µs` |
 
-**Total: 0.27ms**
+**Total: 0.25ms**
 <!--- benchmarking table --->
 
--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,12 +1,66 @@
-// use advent_of_code::BytesResult;
+use advent_of_code::BytesResult;
+use nom::bytes::complete::{tag, take};
 
 advent_of_code::solution!(4);
 
+pub fn x(l: usize, input: &[u8]) -> BytesResult<()> {
+    let (rem, _) = tag(b"X")(input)?;
+    let (rem, _) = take(l)(rem)?;
+    let (rem, _) = tag(b"M")(rem)?;
+    let (rem, _) = take(l)(rem)?;
+    let (rem, _) = tag(b"A")(rem)?;
+    let (rem, _) = take(l)(rem)?;
+    let (rem, _) = tag(b"S")(rem)?;
+
+    Ok((rem, ()))
+}
+pub fn s(l: usize, input: &[u8]) -> BytesResult<()> {
+    let (rem, _) = tag(b"S")(input)?;
+    let (rem, _) = take(l)(rem)?;
+    let (rem, _) = tag(b"A")(rem)?;
+    let (rem, _) = take(l)(rem)?;
+    let (rem, _) = tag(b"M")(rem)?;
+    let (rem, _) = take(l)(rem)?;
+    let (rem, _) = tag(b"X")(rem)?;
+
+    Ok((rem, ()))
+}
+
+fn xmas(input: &[u8], ll: usize) -> BytesResult<'_, u32> {
+    let mut acc = 0;
+    for i in [0, ll, ll - 1, ll + 1] {
+        if x(i, input).is_ok() {
+            acc += 1;
+        }
+        if s(i, input).is_ok() {
+            acc += 1
+        }
+    }
+    Ok((&input[1..], acc))
+}
+
+pub fn part_one_nom(input: &str) -> Option<u32> {
+    let line_len = input.lines().next().unwrap().len();
+    let mut input = input.as_bytes();
+
+    let mut acc = 0;
+
+    while !input.is_empty() {
+        if let Ok((_, res)) = xmas(input, line_len) {
+            acc += res;
+        }
+        input = &input[1..]
+    }
+
+    Some(acc)
+}
+
+// much faster than the solution wiht nom ,sadly.
 pub fn part_one(input: &str) -> Option<u32> {
     let line_len = input.lines().next().unwrap().len() + 1;
     let input = input.as_bytes();
 
-    let mut _hor = input.windows(4).fold(0, |acc, window| {
+    let mut acc = input.windows(4).fold(0, |acc, window| {
         if window == b"XMAS" || window == b"SAMX" {
             acc + 1u32
         } else {
@@ -14,7 +68,9 @@ pub fn part_one(input: &str) -> Option<u32> {
         }
     });
 
+    let diagonal = line_len - 1;
     for i in 0..input.len() - line_len * 3 {
+        // if input[i] != b'X' && input[i] !=b'S' { continue;}
         let vert = [
             input[i],
             input[i + line_len],
@@ -22,20 +78,16 @@ pub fn part_one(input: &str) -> Option<u32> {
             input[i + line_len * 3],
         ];
         if &vert == b"XMAS" || &vert == b"SAMX" {
-            _hor += 1;
+            acc += 1;
         }
-    }
-
-    let diagonal = line_len - 1;
-    for i in 0..input.len() - line_len * 3 {
-        let vert = [
+        let diag = [
             input[i],
             input[i + diagonal],
             input[i + diagonal * 2],
             input[i + diagonal * 3],
         ];
-        if &vert == b"XMAS" || &vert == b"SAMX" {
-            _hor += 1;
+        if &diag == b"XMAS" || &diag == b"SAMX" {
+            acc += 1;
         }
     }
 
@@ -48,11 +100,11 @@ pub fn part_one(input: &str) -> Option<u32> {
             input[i + diagonal * 3],
         ];
         if &vert == b"XMAS" || &vert == b"SAMX" {
-            _hor += 1;
+            acc += 1;
         }
     }
 
-    Some(_hor)
+    Some(acc)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -62,6 +114,9 @@ pub fn part_two(input: &str) -> Option<u32> {
     let mut acc = 0;
 
     for i in line_len + 1..input.len() - line_len - 1 {
+        if input[i] != b'A' {
+            continue;
+        }
         let dia = [input[i - line_len - 1], input[i], input[i + line_len + 1]];
         let di2 = [input[i - line_len + 1], input[i], input[i + line_len - 1]];
 

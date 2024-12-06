a1c1e4a4b301aaf2c148a83de26dea541ee08d47
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2024-12-06 23:03:18 +0300

    add nom_locate types

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@ pub mod template;
 
 // Use this file to add helper functions and additional modules.
 
-pub type BytesResult<'a, T> = nom::IResult<&'a [u8], T, ()>;
+pub type BytesResult<'a, T, I = &'a [u8]> = nom::IResult<I, T, ()>;
+
+pub type Span<'a, X = ()> = nom_locate::LocatedSpan<&'a [u8], X>;
+pub type SpanResult<'a, T> = BytesResult<'a, T, Span<'a>>;
 
 pub mod bitset;

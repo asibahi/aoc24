52a7ecf89bad3caca67754b96c92b1dbb779eb30
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2024-12-06 13:24:49 +0300

    code cleanup

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -14,6 +14,24 @@ enum Direction {
     Left = 4,
     Down = 8,
 }
+impl Direction {
+    fn forward(self, (x, y): (usize, usize)) -> (usize, usize) {
+        match self {
+            Direction::Up => (x - 1, y),
+            Direction::Right => (x, y + 1),
+            Direction::Left => (x, y - 1),
+            Direction::Down => (x + 1, y),
+        }
+    }
+    fn rotate(self) -> Self {
+        match self {
+            Self::Up => Self::Right,
+            Self::Right => Self::Down,
+            Self::Left => Self::Up,
+            Self::Down => Self::Left,
+        }
+    }
+}
 
 #[derive(Debug)]
 enum Element {
@@ -54,23 +72,10 @@ pub fn part_one(input: &str) -> Option<u32> {
     let mut dir = Direction::Up;
 
     loop {
-        let (x, y) = guard_loc;
-        let forward = match dir {
-            Direction::Up => (x - 1, y),
-            Direction::Right => (x, y + 1),
-            Direction::Left => (x, y - 1),
-            Direction::Down => (x + 1, y),
-        };
+        let forward = dir.forward(guard_loc);
 
         match map.get(&forward) {
-            Some(Element::Obstacle) => {
-                dir = match dir {
-                    Direction::Up => Direction::Right,
-                    Direction::Right => Direction::Down,
-                    Direction::Left => Direction::Up,
-                    Direction::Down => Direction::Left,
-                };
-            }
+            Some(Element::Obstacle) => dir = dir.rotate(),
             Some(Element::Unvisited) => {
                 counter += 1;
                 guard_loc = forward;
@@ -86,7 +91,7 @@ pub fn part_one(input: &str) -> Option<u32> {
 
 pub fn part_two(input: &str) -> Option<u32> {
     let mut map = HashMap::new();
-    let mut guard_loc = (0, 0);
+    let mut start = (0, 0);
 
     for (idx, line) in input.lines().enumerate() {
         let idx = idx + 1;
@@ -96,7 +101,7 @@ pub fn part_two(input: &str) -> Option<u32> {
                 '#' => map.insert((idx, jdx), Element2::Obstacle),
                 '.' => map.insert((idx, jdx), Element2::Unvisited),
                 '^' => {
-                    guard_loc = (idx, jdx);
+                    start = (idx, jdx);
                     map.insert((idx, jdx), Element2::Visited(Direction::Up as u8))
                 }
                 _ => unreachable!(),
@@ -105,35 +110,19 @@ pub fn part_two(input: &str) -> Option<u32> {
     }
 
     let mut set = HashSet::with_capacity(6000);
-    {
-        let mut guard_loc = guard_loc;
-        let mut dir = Direction::Up;
-        loop {
-            let (x, y) = guard_loc;
-            let forward = match dir {
-                Direction::Up => (x - 1, y),
-                Direction::Right => (x, y + 1),
-                Direction::Left => (x, y - 1),
-                Direction::Down => (x + 1, y),
-            };
-
-            match map.get(&forward) {
-                Some(Element2::Obstacle) => {
-                    dir = match dir {
-                        Direction::Up => Direction::Right,
-                        Direction::Right => Direction::Down,
-                        Direction::Left => Direction::Up,
-                        Direction::Down => Direction::Left,
-                    };
-                }
-                Some(Element2::Unvisited) => {
-                    guard_loc = forward;
-                    set.insert(forward);
-                    map.insert(forward, Element2::HotPath);
-                }
-                None => break,
-                _ => guard_loc = forward,
+    let mut guard_loc = start;
+    let mut dir = Direction::Up;
+    loop {
+        let forward = dir.forward(guard_loc);
+        match map.get(&forward) {
+            None => break,
+            Some(Element2::Obstacle) => dir = dir.rotate(),
+            Some(Element2::Unvisited) => {
+                guard_loc = forward;
+                set.insert(forward);
+                map.insert(forward, Element2::HotPath);
             }
+            _ => guard_loc = forward,
         }
     }
 
@@ -144,43 +133,27 @@ pub fn part_two(input: &str) -> Option<u32> {
 
             map.insert((*idx, *jdx), Element2::Obstacle);
 
-            // ==
             let mut dir = Direction::Up;
-            let mut guard_loc = guard_loc;
+            let mut guard_loc = start;
 
             loop {
-                let (x, y) = guard_loc;
-                let forward = match dir {
-                    Direction::Up => (x - 1, y),
-                    Direction::Right => (x, y + 1),
-                    Direction::Left => (x, y - 1),
-                    Direction::Down => (x + 1, y),
-                };
+                let forward = dir.forward(guard_loc);
 
                 match map.get_mut(&forward) {
-                    Some(Element2::Obstacle) => {
-                        dir = match dir {
-                            Direction::Up => Direction::Right,
-                            Direction::Right => Direction::Down,
-                            Direction::Left => Direction::Up,
-                            Direction::Down => Direction::Left,
-                        };
-                    }
+                    Some(Element2::Visited(v)) if *v & dir as u8 > 0 => return true,
+                    None => return false,
+
+                    Some(Element2::Obstacle) => dir = dir.rotate(),
                     Some(Element2::Unvisited | Element2::HotPath) => {
                         guard_loc = forward;
                         map.insert(forward, Element2::Visited(dir as u8));
                     }
                     Some(Element2::Visited(ref mut v)) => {
-                        if *v & dir as u8 > 0 {
-                            return true;
-                        }
                         *v |= dir as u8;
                         guard_loc = forward
                     }
-                    None => return false,
                 }
             }
-            //==
         })
         .count();
 
